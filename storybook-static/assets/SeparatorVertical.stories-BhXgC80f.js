ef32309d4ba73acb2fd4b90b55b1a55b360483ba
Kayo Ennrique <kayoennrique@kayoennrique.com>
2024-09-08 21:09:40 -0300

    feat: default values ​​in storybook

---
+++ storybook-static/assets/SeparatorVertical.stories-BhXgC80f.js
@@ -0,0 +1,7 @@
+import{S as o}from"./index-C9y2phuT.js";import"./jsx-runtime-DEdD30eg.js";import"./index-RYns6xqu.js";import"./styled-components.browser.esm-DDAiM4bh.js";const n={title:"Design System/SeparatorVertical",component:o,argTypes:{height:{type:"string"},width:{type:"string"},bgcolor:{type:"string"}},tags:["autodocs"]},r={args:{width:"3px",height:"150px",bgcolor:"#480855"}};var t,e,a;r.parameters={...r.parameters,docs:{...(t=r.parameters)==null?void 0:t.docs,source:{originalSource:`{
+  args: ({
+    width: "3px",
+    height: "150px",
+    bgcolor: "#480855"
+  } as SeparatorVerticalProps)
+}`,...(a=(e=r.parameters)==null?void 0:e.docs)==null?void 0:a.source}}};const g=["Primary"];export{r as Primary,g as __namedExportsOrder,n as default};
