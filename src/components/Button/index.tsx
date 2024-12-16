bf1311be045bdc056b239dae70e59a2705119d81
Kayo Ennrique <kayo.ennrique@hotmail.com.br>
2024-12-16 19:11:06 -0300

    fix: bug fix in Button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -4,50 +4,69 @@ import { ButtonProps } from "./types";
 import * as S from "./styles";
 
 const Button = ({
-    textbutton,
-    onClick,
-    bgcolor,
-    hoverbgcolor,
-    textcolor,
-    icon,
-    widthbutton,
-    borderbutton,
-    paddingbutton,
-    fontweightbutton,
-    boxshadowbutton,
-    textdecoration,
-    textcolorhover,
-    borderadius,
-    fontsize,
-    fontfamily,
-    isblock,
-    ...rest
+  textbutton,
+  onClick,
+  bgcolor,
+  hoverbgcolor,
+  textcolor,
+  icon,
+  widthbutton,
+  borderbutton,
+  paddingbutton,
+  fontweightbutton,
+  boxshadowbutton,
+  textdecoration,
+  textcolorhover,
+  borderadius,
+  fontsize,
+  fontfamily,
+  isblock,
+  ...rest
 }: ButtonProps) => {
-    return (
-        <StyleSheetManager shouldForwardProp={(prop) => prop !== "bgcolor"}>
-            <S.StylizedButton
-                onClick={onClick}
-                textdecoration={textdecoration}
-                textcolor={textcolor}
-                textcolorhover={textcolorhover}
-                bgcolor={bgcolor}
-                hoverbgcolor={hoverbgcolor}
-                boxshadowbutton={boxshadowbutton}
-                borderbutton={borderbutton}
-                widthbutton={widthbutton}
-                paddingbutton={paddingbutton}
-                fontweightbutton={fontweightbutton}
-                fontsize={fontsize}
-                fontfamily={fontfamily}
-                isblock={isblock}
-                borderadius={borderadius}
-                {...rest}
-            >
-                {icon && <span>{icon}</span>}
-                {textbutton}
-            </S.StylizedButton>
-        </StyleSheetManager>
-    );
+  return (
+    <StyleSheetManager
+      shouldForwardProp={(prop) =>
+        ![
+          "bgcolor",
+          "textdecoration",
+          "textcolor",
+          "textcolorhover",
+          "hoverbgcolor",
+          "boxshadowbutton",
+          "borderbutton",
+          "widthbutton",
+          "paddingbutton",
+          "fontweightbutton",
+          "fontsize",
+          "fontfamily",
+          "isblock",
+          "borderadius",
+        ].includes(prop)
+      }
+    >
+      <S.StylizedButton
+        onClick={onClick}
+        textdecoration={textdecoration}
+        textcolor={textcolor}
+        textcolorhover={textcolorhover}
+        bgcolor={bgcolor}
+        hoverbgcolor={hoverbgcolor}
+        boxshadowbutton={boxshadowbutton}
+        borderbutton={borderbutton}
+        widthbutton={widthbutton}
+        paddingbutton={paddingbutton}
+        fontweightbutton={fontweightbutton}
+        fontsize={fontsize}
+        fontfamily={fontfamily}
+        isblock={isblock}
+        borderadius={borderadius}
+        {...rest}
+      >
+        {icon && <span>{icon}</span>}
+        {textbutton}
+      </S.StylizedButton>
+    </StyleSheetManager>
+  );
 };
 
 export default Button;
