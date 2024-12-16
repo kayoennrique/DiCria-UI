89264470020bad771065b29504064c6c0855fab1
Kayo Ennrique <kayo.ennrique@hotmail.com.br>
2024-12-16 19:13:35 -0300

    update

---
+++ storybook-static/assets/index-D9A3qrMb.js
@@ -0,0 +1,21 @@
+import{j as t}from"./jsx-runtime-CkxqCPlQ.js";import{d as q,Y as T}from"./styled-components.browser.esm-WJtJfqrr.js";const w=q.button`
+    transition: 0.3s;
+    cursor: pointer;
+    text-decoration: ${e=>e.textdecoration};
+    box-shadow: ${e=>e.boxshadowbutton};
+    background-color: ${e=>e.bgcolor};
+    padding: ${e=>e.paddingbutton};
+    border: ${e=>e.borderbutton};
+    border-radius: ${e=>e.borderadius};
+    width: ${e=>e.widthbutton};
+    color: ${e=>e.textcolor};
+    font-weight: ${e=>e.fontweightbutton};
+    font-size: ${e=>e.fontsize};
+    font-family: ${e=>e.fontfamily};
+    font-style: ${e=>e.fontstyle};
+
+    &:hover {
+        background-color: ${e=>e.hoverbgcolor};
+        color: ${e=>e.textcolorhover};
+    }
+`,$=({textbutton:e,onClick:r,bgcolor:n,hoverbgcolor:i,textcolor:s,icon:o,widthbutton:d,borderbutton:a,paddingbutton:c,fontweightbutton:u,boxshadowbutton:l,textdecoration:p,textcolorhover:f,borderadius:b,fontsize:g,fontfamily:m,isblock:y,...h})=>t.jsx(T,{shouldForwardProp:x=>!["bgcolor","textdecoration","textcolor","textcolorhover","hoverbgcolor","boxshadowbutton","borderbutton","widthbutton","paddingbutton","fontweightbutton","fontsize","fontfamily","isblock","borderadius"].includes(x),children:t.jsxs(w,{onClick:r,textdecoration:p,textcolor:s,textcolorhover:f,bgcolor:n,hoverbgcolor:i,boxshadowbutton:l,borderbutton:a,widthbutton:d,paddingbutton:c,fontweightbutton:u,fontsize:g,fontfamily:m,isblock:y,borderadius:b,...h,children:[o&&t.jsx("span",{children:o}),e]})});$.__docgenInfo={description:"",methods:[],displayName:"Button",props:{fontweightbutton:{required:!1,tsType:{name:"string"},description:""},textbutton:{required:!1,tsType:{name:"string"},description:""},textcolor:{required:!1,tsType:{name:"string"},description:""},textcolorhover:{required:!1,tsType:{name:"string"},description:""},textdecoration:{required:!1,tsType:{name:"string"},description:""},bgcolor:{required:!1,tsType:{name:"string"},description:""},hoverbgcolor:{required:!1,tsType:{name:"string"},description:""},boxshadowbutton:{required:!1,tsType:{name:"string"},description:""},borderbutton:{required:!1,tsType:{name:"string"},description:""},paddingbutton:{required:!1,tsType:{name:"string"},description:""},widthbutton:{required:!1,tsType:{name:"string"},description:""},isblock:{required:!1,tsType:{name:"boolean"},description:""},onClick:{required:!1,tsType:{name:"signature",type:"function",raw:"() => void",signature:{arguments:[],return:{name:"void"}}},description:""},icon:{required:!1,tsType:{name:"ReactReactNode",raw:"React.ReactNode"},description:""},borderadius:{required:!1,tsType:{name:"string"},description:""},fontsize:{required:!1,tsType:{name:"string"},description:""},fontfamily:{required:!1,tsType:{name:"string"},description:""},fontstyle:{required:!1,tsType:{name:"string"},description:""}}};export{$ as B};
